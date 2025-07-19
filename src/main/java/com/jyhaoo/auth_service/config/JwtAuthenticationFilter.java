45a6d47e1874aa4195c288a49dcbe706512d8775
jyhaoo <jyhaoo1998@gmail.com>
2025-07-19 12:03:28 -0700

    feat: add jwtAuthenticationFilter to intercept HTTP requests and validate token signatures

---
+++ src/main/java/com/jyhaoo/auth_service/config/JwtAuthenticationFilter.java
@@ -0,0 +1,81 @@
+package com.jyhaoo.auth_service.config;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.jyhaoo.auth_service.service.JwtService;
+
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(
+        JwtService jwtService,
+        HandlerExceptionResolver handlerExceptionResolver,
+        UserDetailsService userDetailsService) {
+            this.jwtService = jwtService;
+            this.handlerExceptionResolver = handlerExceptionResolver;
+            this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal (
+        @Nonnull HttpServletRequest request, 
+        @Nonnull HttpServletResponse response, 
+        @Nonnull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response); // Continue the filter chain if no JWT is present
+            return;
+        }
+
+        try {
+            final String jwt = authHeader.substring(7);
+            final String userEmail = jwtService.extractUsername(jwt);
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (userEmail != null && authentication == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+                
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        authentication, 
+                        null,
+                        userDetails.getAuthorities()
+                    );
+
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (Exception exception) {
+            handlerExceptionResolver.resolveException(request, response, null, exception);
+        }
+    }
+
+}
