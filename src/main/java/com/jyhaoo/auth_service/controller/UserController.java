0870299e29b09576446dcb7de4214d8f3b7f780a
jyhaoo <jyhaoo1998@gmail.com>
2025-07-30 15:58:52 -0700

    feat: add user controller

---
+++ src/main/java/com/jyhaoo/auth_service/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.jyhaoo.auth_service.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jyhaoo.auth_service.model.User;
+import com.jyhaoo.auth_service.service.UserService;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authicatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> allUsers() {
+        List <User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+}
